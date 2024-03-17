210e3208f284e46cffd5cbbd98cfc298b91eac9a
Omkar <omkarchebale0@gmail.com>
2024-03-17 17:53:04 +0530

    update readme

--- temp.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    // Print "Hello, World!" to the console
-    cout << "Hello, World!" << endl;
-    cout << "Hello, World!2" << endl;
-    return 0;
-}
